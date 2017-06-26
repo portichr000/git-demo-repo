9d84aef4397334340184a514cffe5b394502f6f2
portichr000 <chris6por@gmail.com>
2017-06-26 14:48:58 +0000

    intergrated technologies model

--- AppName/db/seeds.rb
+++ AppName/db/seeds.rb
@@ -44,3 +44,14 @@ puts "10 blog posts created"
     
   
   puts "9 portfolio items created"
+  
+  3.times do |technology|
+    Portfolio.last.technologies.create!(
+      name: "Technology#{technology}",
+      
+      )
+    
+  end
+    
+    
+    puts "3 technology created"
