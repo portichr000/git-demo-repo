e23f30c78780262a0809795ec9d09e13a56f0f2b
portichr000 <chris6por@gmail.com>
2017-07-25 17:43:17 +0000

    built out initial form and partioal call foor ocments along with controller actions

--- AppName/app/controllers/blogs_controller.rb
+++ AppName/app/controllers/blogs_controller.rb
@@ -13,6 +13,9 @@ access all: [:show, :index], user: {except: [:destroy, :toggle_status, :new, :cr
   # GET /blogs/1
   # GET /blogs/1.json
   def show
+    @blog = Blog.includes(:comments).friendly.find(params[:id])
+     @comment = Comment.new
+    
     @page_title = @blog.title
     @seo_keywords = @blog.body
   end
