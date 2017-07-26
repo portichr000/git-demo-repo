e90f9be2e717bcaf53b0fbcc35ecc231b4eb5af5
portichr000 <chris6por@gmail.com>
2017-07-26 15:08:17 +0000

    Created a commetn broadcast, config websocket route

--- AppName/config/routes.rb
+++ AppName/config/routes.rb
@@ -1,22 +1,21 @@
 Rails.application.routes.draw do
-  resources :comments
-  devise_for :users, path: '', path_names: {sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
-  resources :portfolios, except: [:show] do 
-    put :sort, on: :collection
-  end
-  get 'angular-items', to: 'portfolios#angular'
-  get 'portfolios/:id', to: 'portfolios#show', as: 'portfolio_show'
-  
-
-
-  get 'about-me', to: 'pages#about'
-  get 'contact', to: 'pages#contact'
-  
-  resources:blogs do 
-    member do
-      get:toggle_status
-   end
-end
-
-   root to:'pages#home'
+    devise_for :users, path: '', path_names: {sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
+    resources :portfolios, except: [:show] do 
+      put :sort, on: :collection
+    end
+    get 'angular-items', to: 'portfolios#angular'
+    get 'portfolios/:id', to: 'portfolios#show', as: 'portfolio_show'
+    
+    get 'about-me', to: 'pages#about'
+    get 'contact', to: 'pages#contact'
+    
+    resources:blogs do 
+      member do
+        get:toggle_status
+      end
+    end
+    
+    mount ActionCable.server => '/cable'
+    
+     root to:'pages#home'
 end
